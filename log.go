85259b5ee0ac4a8e01732885493e059c4c333059
Antoine Grondin <antoinegrondin@gmail.com>
2016-02-26 13:02:28 +0530

    smarter interface handling, expose package lvl KV func

--- log.go
+++ log.go
@@ -16,11 +16,24 @@ func newLog(w io.Writer) *Log {
 	return &Log{ctx: log.NewContext(log.NewJSONLogger(w))}
 }
 
-func (l *Log) KV(k string, v interface{}) *Log { return &Log{ctx: l.ctx.With(k, v)} }
-func (l *Log) Err(err error) *Log              { return l.KV("err", err) }
-func (l *Log) Error(msg string)                { l.log("error", msg) }
-func (l *Log) Info(msg string)                 { l.log("info", msg) }
-func (l *Log) Fatal(msg string)                { l.log("fatal", msg); os.Exit(1) }
+func (l *Log) KV(k string, v interface{}) *Log {
+	switch s := v.(type) {
+	case interface {
+		String() string
+	}:
+		v = s.String()
+	case interface {
+		GoString() string
+	}:
+		v = s.GoString()
+	}
+	return &Log{ctx: l.ctx.With(k, v)}
+}
+
+func (l *Log) Err(err error) *Log { return l.KV("err", err) }
+func (l *Log) Error(msg string)   { l.log("error", msg) }
+func (l *Log) Info(msg string)    { l.log("info", msg) }
+func (l *Log) Fatal(msg string)   { l.log("fatal", msg); os.Exit(1) }
 
 func (l *Log) log(lvl, msg string) {
 	err := l.ctx.Log(
@@ -34,8 +47,8 @@ func (l *Log) log(lvl, msg string) {
 	}
 }
 
-func KV(k string, v interface{}) *Log { return logger.KV(k, v) }
 func Err(err error) *Log              { return logger.Err(err) }
 func Error(msg string)                { logger.log("error", msg) }
 func Info(msg string)                 { logger.log("info", msg) }
 func Fatal(msg string)                { logger.log("fatal", msg); os.Exit(1) }
+func KV(k string, v interface{}) *Log { return logger.KV(k, v) }
